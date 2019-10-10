da7aa7b05289873d3c5e8ef26976ef3720d03586
nr7195a <nr7195a@student.american.edu>
2019-10-10 16:25:14 -0400

    Link Change

---
+++ Exercises/Game02/Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public CharacterHealth CH;
+
+    public Text countdownText;
+    private float countupChange = 1;
+    float currentTime = 0f;
+    float startingTime = 0f;
+    public Text tempTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        currentTime = startingTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currentTime += countupChange * Time.deltaTime;
+        countdownText.text = currentTime.ToString();
+        if (CH.CurrentHealth <= 0)
+        {
+            countupChange = 0f;
+
+        }
+
+
+    }
+}
---
+++ Exercises/Game1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text endGame;
+
+    private int randomNum; 
+
+    private Rigidbody rb;
+    private int count;
+
+
+    void Start()
+    {
+
+        randomNum = Random.Range(10, 20);
+
+        rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SetCountText();
+
+        winText.text = "";
+
+        endGame.text = "";
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+        if(other.transform.tag == "gor") {
+            endGame.text = "His Bananas!!!";
+            other.gameObject.SetActive(false);
+            
+        }
+        if (other.transform.tag == "gor")
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+
+
+    void SetCountText()
+    {
+
+        countText.text = "Count: " + count.ToString();
+        if (count >= randomNum)
+        {
+            winText.text = "Your Bananas!!!";
+            speed = 0;
+
+        }
+
+    }
+
+}
+
+
+
+       
