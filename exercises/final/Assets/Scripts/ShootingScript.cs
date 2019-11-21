2d5ff8a4aeccce09f190418bb7a68b49e17a8395
nr7195a <nr7195a@student.american.edu>
2019-11-21 17:04:17 -0500

    Firing Bullet Test

---
+++ exercises/final/Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+
+    public GameObject Bullet_Emitter;
+    public GameObject Bullet;
+    public float Bullet_Forward_Force;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            GameObject Temporary_Bullet_Handler;
+            Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+            Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+         
+            Rigidbody Temporary_RigidBody;
+            Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+          
+            Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
+
+            Destroy(Temporary_Bullet_Handler, 10.0f);
+        }
+        
+    }
+}
