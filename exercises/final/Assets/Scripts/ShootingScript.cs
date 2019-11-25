a9b3b784a196375136586782559e5d3cc8fe9d7a
Nate <nr7195a@student.american.edu>
2019-11-25 13:40:53 -0500

    BadGuy Push

--- exercises/final/Assets/Scripts/ShootingScript.cs
+++ exercises/final/Assets/Scripts/ShootingScript.cs
@@ -8,6 +8,8 @@ public class ShootingScript : MonoBehaviour
     public GameObject Bullet_Emitter;
     public GameObject Bullet;
     public float Bullet_Forward_Force;
+    private bool allowFire = true;
+    public ParticleSystem effectOfGun;
     
     void Start()
     {
@@ -18,22 +20,37 @@ public class ShootingScript : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKey(KeyCode.Mouse0))
+        
+        
+
+        if (allowFire == true && Input.GetKey(KeyCode.Mouse0))
         {
+            effectOfGun.Emit(1);
+            StartCoroutine(WaitASecond());
             GameObject Temporary_Bullet_Handler;
             Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
 
-            Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+            // Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 180);
 
-         
+
             Rigidbody Temporary_RigidBody;
             Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
 
-          
+
             Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
 
             Destroy(Temporary_Bullet_Handler, 10.0f);
+
         }
         
     }
+    IEnumerator WaitASecond()
+    {
+        allowFire = false;
+        yield return new WaitForSeconds(1);
+        allowFire = true;
+       
+    }
+
+
 }
